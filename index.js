b79c0c98071f79830e1b11817bf68d929e0492f4
Zachary <zachary.lai@tinklabs.com>
2018-04-16 16:52:50 +0800

    auth bug fixed

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ var Salesforce = function(settings) {
     var config  = settings;
     var cacheTimeout =  (!settings || !settings.timeout ) ? 15 * 60 * 1000 : settings.timeout; // 15 minutes in milliseconds
     var isSandbox = (config && config.isSandbox);
-
+    
     var cacheOptions = {timeout: cacheTimeout};
-
+    var cacheUser = new Cache(cacheOptions);
     Object.defineProperty(this, "cacheAuth", {
         enumerable: false,
         configurable: false,
@@ -23,7 +23,19 @@ var Salesforce = function(settings) {
         value: new Cache(cacheOptions)
     });
 
-    var cacheUser = new Cache(cacheOptions);
+    self.cacheAuth.on("expired", function (keyValue) {
+        // removes entry from user cache
+        if (!keyValue || !keyValue.value) return;
+        cacheUser.remove(keyValue.value.username);
+        if (typeof keyValue.value.connection === 'function') keyValue.value.connection.logout();
+    });
+
+    // jwtFlow must use a new connection on each request, because no token-refresh procedure was implemented,
+    // so when jwtFlow was configured, the connector will return always the same auth token
+    // in order to simplify the cache managment.
+
+    var jwtFlowAuthToken = uuid.v4(); // Internal auth token;
+    var auth;
 
     this.authenticate = function (credentials, cb) {
 
@@ -37,52 +49,29 @@ var Salesforce = function(settings) {
         if (typeof cb !== 'function') return cb(new Error("'cb' argument is invalid."));
         if (!credentials || typeof credentials !== 'object') return  cb(new Error("'credentials' argument is invalid."));
 
-        var account;
-        var secret;
-        var sfConnection;
+        // auth using JWT Flow
+        if (credentials.useOAuthJwtFlow || config.useOAuthJwtFlow) return authenticateUsingJwtFlow(credentials, function (err, cn) {
+            if (err) return cb(err);
+            cn.logout();
+            return cb(null, { auth: jwtFlowAuthToken });
+        });
 
-        if (isSandbox) {
-            sfConnection = new SF.Connection({ loginUrl: 'https://test.salesforce.com' });
-        } else {
-            sfConnection = new SF.Connection();
+        account = credentials.username || config.username; 
+        secret = credentials.password || config.password;           
+
+       // Validates user credentials
+        if (!account || typeof (account) !== 'string') return cb(new Error("'username' property is missing or invalid."));
+        if (!secret || typeof (secret) !== 'string') return cb(new Error("'password' property is missing or invalid."));
+
+        // try to find if a connection is not in the cache
+        var auth = cacheUser.get(account);
+        if (auth) {
+            var item = self.cacheAuth.get(auth);
+            if (item && item.password === secret) return cb(null, { auth: auth });
+            return cb (new Error("invalid username or password."));
         }
 
-        if (credentials.useOAuthJwtFlow) {
-
-            if (!credentials.privateKey) {
-                cb(new Error('credentials.privateKey is required when using OAuth JWT Bearer Flow.'));
-                return;
-            };
-
-            if (!credentials.clientId) {
-                cb(new Error('credentials.clientId is required when using OAuth JWT Bearer Flow.'));
-                return;
-            };
-
-            if (!credentials.actAsUsername) {
-                cb(new Error('credentials.username is required when using OAuth JWT Bearer Flow.'));
-                return;
-            };
-
-            jwtflow(credentials.clientId, credentials.privateKey, credentials.actAsUsername, isSandbox, function(err, accessToken) {
-                if (err) {
-                    cb(err);
-                    return;
-                }
-
-                sfConnection = new SF.Connection();
-
-                sfConnection.initialize({
-                  instanceUrl: 'https://' + credentials.loginHost,
-                  accessToken: accessToken
-                });
-
-                cb(null, sfConnection);
-                return;
-            });
-
-            return;
-        };
+        var sfConnection;
 
         if (credentials.oauth2 && typeof credentials.oauth2 == 'object') {
             if (typeof (credentials.oauth2.clientSecret) !== 'string') return cb(new Error("'oauth2.clientSecret' property is invalid."));
@@ -92,42 +81,44 @@ var Salesforce = function(settings) {
             } else {
                 sfConnection = new SF.Connection(credentials.oauth2);
             }
+        } else {
+            var sfOptions = {};
+            if (isSandbox) sfOptions.loginUrl = 'https://test.salesforce.com';
+            sfConnection = new SF.Connection(sfOptions);
         }
 
-        // Validates user credentials
-        if (!credentials.username || typeof (credentials.username) !== 'string') return cb(new Error("'username' property is missing or invalid."));
-        if (!credentials.password || typeof (credentials.password) !== 'string') return cb(new Error("'password' property is missing or invalid."));
-
-        account = credentials.username || config.username; 
-        secret = credentials.password || config.password;           
-
-        var auth = cacheUser.get(account);
-        if (auth) {
-            var item = self.cacheAuth.get(auth);
-            if (item && item.password === secret) return cb(null, item.connection);
-        }
-
-        sfConnection.login(account, secret, function(error, userInfo) {
-
-            if (error) {
-                return cb(error,null);
-            };
+        sfConnection.login(account, secret, function (err, userInfo) {
+            if (err) return cb(err);
             
-            var auth = uuid.v4(); // Internal auth token 
             var item = {
+                auth: uuid.v4(), // Internal auth token 
                 username: account,
                 password: secret,
                 connection: sfConnection
             };
 
-            self.cacheAuth.set(auth, item);  
-            cacheUser.set(account, auth);
+            self.cacheAuth.set(item.auth, item);  
+            cacheUser.set(account, item.auth);
 
-            cb(null, sfConnection);
+            cb(null, { auth: item.auth });
         });
     };
 
     var getSFConnection = function (options, cb) {
+
+        if (options.auth) {
+            
+            if (options.auth === jwtFlowAuthToken) {
+                if (!options.useOAuthJwtFlow && !config.useOAuthJwtFlow) return cb(new Error("Invalid 'auth' token."));
+                authenticateUsingJwtFlow(options, cb);
+                return;
+            }
+
+            // searchs auth token at the cache
+            var item = self.cacheAuth.get(options.auth);
+            if (!item) return cb(new Error("Invalid 'auth' token."));
+            return cb(null, item.connection, options.auth);
+        }
 
         // defaults for credentials
         var credentials = options.credentials || config;
@@ -140,92 +131,161 @@ var Salesforce = function(settings) {
                 return c.type == claimType; 
             })[0] || {}).value;
 
-            if (!credentials.actAsUsername) {
-                cb(new Error('Claim type "' + claimType + '" was not found in user claims.'));
-                return;
-            };
+            if (!credentials.actAsUsername) return cb(new Error('Claim type "' + claimType + '" was not found in user claims.'));
         }
 
-        self.authenticate(credentials, cb);
+        // auth using JWT Flow
+        if (credentials.useOAuthJwtFlow || config.useOAuthJwtFlow) return authenticateUsingJwtFlow(credentials, cb);
+
+        self.authenticate(credentials, function (err, data) {
+            if (err) return cb(err);
+
+            var item = self.cacheAuth.get(data.auth);  
+            cb(null, item.connection, data.auth);
+        });
+    };
+
+    var authenticateUsingJwtFlow = function (credentials, cb) {
+
+        var privateKey = credentials.privateKey || config.privateKey;
+        if (!privateKey) {
+            cb(new Error('credentials.privateKey is required when using OAuth JWT Bearer Flow.'));
+            return;
+        };
+
+        var clientId = credentials.clientId || config.clientId;
+        if (!clientId) {
+            cb(new Error('credentials.clientId is required when using OAuth JWT Bearer Flow.'));
+            return;
+        };
+
+        var actAsUsername = credentials.actAsUsername || config.actAsUsername;
+        if (!actAsUsername) {
+            cb(new Error('credentials.username is required when using OAuth JWT Bearer Flow.'));
+            return;
+        };
+
+        jwtflow(clientId, privateKey, actAsUsername, isSandbox, config.customTokenEndpoint, function (err, accessToken) {
+            if (err) return cb(err);
+
+            var options = {
+              instanceUrl: 'https://' + (credentials.loginHost || config.loginHost),
+              accessToken: accessToken
+            };
+
+            var sfConnection = new SF.Connection();
+            sfConnection.initialize(options);
+            cb(null, sfConnection, jwtFlowAuthToken);
+        });
+    };
+
+    this.close = function(cb) {
+        cb = cb || function(){};
+        var keys = self.cacheAuth.keys;
+        var count = keys.length;
+
+        cacheUser.clean();
+        if (!count) return cb();
+
+        keys
+            .forEach(function (key) {
+                var item = self.cacheAuth.remove(key);
+                if (item) { 
+                    item.connection.logout(function() {
+                        if (--count==0) cb();
+                    });
+                } else {
+                    if (--count==0) cb();
+                }
+            });
     };
 
     this.Query = function(options, cb) {
-        getSFConnection(options, function(err, item) {
-            if (err) { return cb(err); };
 
-            if (options.SOSQL) {
-                item.query(options.SOSQL, function(err, result) {
-                    return cb(err, result);
-                });
-            };
-            item.sobject(options.Entity)
-            .find(options.Conditions, options.Fields, options.Options, function(err, result) {
-                return cb(err, result);
-            });
+        getSFConnection(options, function (err, cn) {
+            if (err) return cb(err);
+
+            if (options.SOSQL) return cn.query(options.SOSQL, cb);
+
+            cn
+            .sobject(options.Entity)
+            .find(options.Conditions, options.Fields, options.Options, cb);
+
+            if (auth===jwtFlowAuthToken) cn.logout();
         });
     };
 
     this.Describe = function(options, cb) {
-        getSFConnection(options, function(err, item) {
-            if (err) { return cb(err); };
+        
+        getSFConnection(options, function (err, cn, auth) {
+            if (err) return cb(err);
 
-            item.sobject(options.objectClass).describe(function(err, result) {
-                return cb(err, result);
-            });
+            cn.sobject(options.objectClass).describe(cb);
+
+            if (auth===jwtFlowAuthToken) cn.logout();
         });
     };
 
-    this.DescribeGlobal = function(cb) {
-        getSFConnection(options, function(err, item) {
-            if (err) { return cb(err); };
+    this.DescribeGlobal = function(options, cb) {
+        
+        getSFConnection(options, function (err, cn, auth) {
+            if (err) return cb(err);
 
-            item.describeGlobal(function(err, result) {
-                return cb(err, result);
-            });
+            cn.describeGlobal(cb);
+
+            if (auth===jwtFlowAuthToken) cn.logout();
         });
     };
 
     this.Create = function(options, cb) {
-        getSFConnection(options, function(err, item) {
-            if (err) { return cb(err); };
+        
+        getSFConnection(options, function (err, cn, auth) {
+            if (err) return cb(err);
 
-            item.sobject(options.Entity)
-            .create(options.Details, function(err, result) {
-                return cb(err, result);
-            });
+            cn
+            .sobject(options.Entity)
+            .create(options.Details, cb);
+
+            if (auth===jwtFlowAuthToken) cn.logout();
         });
     };
 
     this.Update = function(options, cb) {
-        getSFConnection(options, function(err, item) {
-            if (err) { return cb(err); };
+        
+        getSFConnection(options, function (err, cn, auth) {
+            if (err) return cb(err);
 
-            item.sobject(options.Entity)
-            .update(options.Details, function(err, result) {
-                return cb(err, result);
-            });
+            cn
+            .sobject(options.Entity)
+            .update(options.Details, cb);
+
+            if (auth===jwtFlowAuthToken) cn.logout();
         });
     };
 
     this.Upsert = function(options, cb) {
-        getSFConnection(options, function(err, item) {
-            if (err) { return cb(err); };
+        
+        getSFConnection(options, function (err, cn, auth) {
+            if (err) return cb(err);
 
-            item.sobject(options.Entity)
-            .upsert(options.Details, options.ExternalIdName, function(err, result) {
-                return cb(err, result);
-            });
+            cn
+            .sobject(options.Entity)
+            .upsert(options.Details, options.ExternalIdName, cb);
+
+            if (auth===jwtFlowAuthToken) cn.logout();
         });
     };
 
     this.Delete = function(options, cb) {
-        getSFConnection(options, function(err, item) {
-            if (err) { return cb(err); };
+        
+        getSFConnection(options, function (err, cn, auth) {
+            if (err) return cb(err);
 
-            item.sobject(options.Entity)
-            .destroy(options.Details, function(err, result) {
-                return cb(err, result);
-            });
+            cn
+            .sobject(options.Entity)
+            .destroy(options.Details, cb);
+
+            if (auth===jwtFlowAuthToken) cn.logout();
         });
     };
 };
